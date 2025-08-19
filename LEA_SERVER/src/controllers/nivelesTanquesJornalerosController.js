c1a666ad2881d100f5035d5603cfa4ad1fe7ea49
Santiiago25 <kevintocora_3d@hotmail.com>
2025-08-18 23:04:11 -0500

    ajustes tanques jornaleros masivo

--- LEA_SERVER/src/controllers/nivelesTanquesJornalerosController.js
+++ LEA_SERVER/src/controllers/nivelesTanquesJornalerosController.js
@@ -74,77 +74,6 @@ export const crearNivel = async (req, res) => {
 
 //carga masiva excel
 
-// export const cargarExcelNivelesTanquesJornaleros = async (req, res) => {
-//   const file = req.files?.excelFile;
-
-//   if (!file) {
-//     return res.status(400).json({ message: 'No se ha cargado ningún archivo' });
-//   }
-
-//   try {
-//     const workbook = XLSX.read(file.data, { type: 'buffer' });
-//     const sheetName = workbook.SheetNames[0];
-//     const worksheet = workbook.Sheets[sheetName];
-//     const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-//     let actualizados = 0;
-//     let noEncontrados = [];
-
-//     // Iterar sobre las filas, comenzando desde la segunda (índice 1) para omitir el encabezado
-//     for (let i = 1; i < data.length; i++) {
-//       const [Tanque, Fecha, Nivel] = data[i];
-
-//       if (!Tanque || !Fecha || Nivel === undefined) {
-//         console.warn('Fila inválida omitida:', data[i]);
-//         continue;
-//       }
-
-//       try {
-//         // Convertir la fecha del Excel al formato 'yyyy-mm-dd'
-//         const fecha = new Date(Fecha);
-//         const fechaString = fecha.toISOString().split('T')[0];
-
-//         // Parsear el nivel, manejando el caso donde el número tiene coma en vez de punto
-//         const nivelParseado = typeof Nivel === 'string' ? parseFloat(Nivel.replace(',', '.')) : Nivel;
-
-//         const filtro = {
-//           NombreTanque: Tanque,
-//           FechaRegistro: fechaString,
-//         };
-
-//         const actualizacion = {
-//           NivelTanque: nivelParseado,
-//         };
-
-//         const resultado = await NivelDiarioJornalerosLogistica.findOneAndUpdate(
-//           filtro,
-//           actualizacion,
-//           { new: true }
-//         );
-
-//         if (resultado) {
-//           console.log(`✔ Actualizado: ${Tanque} - ${fechaString}`);
-//           actualizados++;
-//         } else {
-//           console.warn(`⚠ No encontrado: ${Tanque} - ${fechaString}`);
-//           noEncontrados.push({ Tanque, Fecha: fechaString });
-//         }
-//       } catch (error) {
-//         console.error('⚠ Error procesando fila:', data[i], error.message);
-//       }
-//     }
-
-//     res.status(200).json({
-//       message: 'Archivo procesado.',
-//       actualizados,
-//       noEncontrados,
-//     });
-//   } catch (error) {
-//     console.error('❌ Error al procesar el archivo Excel:', error);
-//     res.status(500).json({ message: 'Error al procesar el archivo Excel', error });
-//   }
-// };
-
 export const cargarExcelNivelesTanquesJornaleros = async (req, res) => {
   const file = req.files?.excelFile;
 
@@ -236,9 +165,7 @@ export const cargarExcelNivelesTanquesJornaleros = async (req, res) => {
 
 // DELETE - Eliminar registros por FechaRegistro
 export const eliminarPorFechaRegistro = async (req, res) => {
-  const { FechaRegistro } = req.body; // O puedes usar req.query.FechaRegistro si prefieres query params
-
-  console.log("fecharegistro que llega:", FechaRegistro);
+  const { FechaRegistro } = req.body; 
   
   if (!FechaRegistro) {
     return res.status(400).json({ message: 'Debe proporcionar el campo FechaRegistro.' });
