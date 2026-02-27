e4e6a462fb8b4a5eac16ee87c68247e646e514a7
ambiocomAPIs <ambiocomambientedepruebas@gmail.com>
2026-02-27 15:14:56 -0500

    nuevo deploy

--- LEA_SERVER/src/keys/key.js
+++ LEA_SERVER/src/keys/key.js
@@ -8,4 +8,4 @@ const DB_NAME = configuraciones.DB_NAME;
 
 export const URI = `mongodb+srv://${configuraciones.DB_CREDENTIALS_USER}:${configuraciones.DB_CREDENTIALS_PASSWORD}` +
   `@cluster0.l0ndbxu.mongodb.net/${DB_NAME}` +
-  `?retryWrites=true&w=majority&appName=Cluster0`;
+  `?retryWrites=true&w=majority&appName=Cluster0`; 
