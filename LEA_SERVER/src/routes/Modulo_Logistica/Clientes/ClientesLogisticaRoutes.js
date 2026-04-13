bda330dbff562c6892ce8af2931198fb39a62363
ambiocomAPIs <ambiocomambientedepruebas@gmail.com>
2026-04-13 09:05:44 -0500

    rutas protegidas backend clientes logistica

--- LEA_SERVER/src/routes/Modulo_Logistica/Clientes/ClientesLogisticaRoutes.js
+++ LEA_SERVER/src/routes/Modulo_Logistica/Clientes/ClientesLogisticaRoutes.js
@@ -12,9 +12,9 @@ import { requireAuth, requireRole } from "../../../middlewares/auth.middleware.j
 const router = Router();
 
 router.get("/",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), getClientes);
-router.get("/:id", getClienteById);
-router.post("/", createCliente);
-router.put("/:id", updateCliente);
-router.delete("/:id", deleteCliente);
+router.get("/:id",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), getClienteById);
+router.post("/",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), createCliente);
+router.put("/:id",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), updateCliente);
+router.delete("/:id",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), deleteCliente);
 
 export default router;
