d1ee110d8e1deab9d7bd013f92eca0131d2af56a
ambiocomAPIs <ambiocomambientedepruebas@gmail.com>
2026-09-12 14:04:33 -0500

    ajuste rol bascula authorized

--- LEA_SERVER/src/routes/Modulo_Logistica/Clientes/ClientesLogisticaRoutes.js
+++ LEA_SERVER/src/routes/Modulo_Logistica/Clientes/ClientesLogisticaRoutes.js
@@ -11,8 +11,8 @@ import { requireAuth, requireRole } from "../../../middlewares/auth.middleware.j
 
 const router = Router();
 
-router.get("/",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), getClientes);
-router.get("/:id",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), getClienteById);
+router.get("/",requireAuth, requireRole("developer","usuariobascula", "liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), getClientes);
+router.get("/:id",requireAuth, requireRole("developer","usuariobascula","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), getClienteById);
 router.post("/",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), createCliente);
 router.put("/:id",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), updateCliente);
 router.delete("/:id",requireAuth, requireRole("developer","liderlogistica","auxiliarlogistica1", "auxiliarlogistica2", "torrecontrollogistica"), deleteCliente);
--- LEA_SERVER/src/routes/Modulo_Logistica/DespachoAlcoholesLogisticaRoutes.js
+++ LEA_SERVER/src/routes/Modulo_Logistica/DespachoAlcoholesLogisticaRoutes.js
@@ -63,6 +63,7 @@ router.post(
   requireAuth,
   requireRole(
     "developer",
+    "usuariobascula",
     "liderlogistica",
     "laboratorio",
     "gerente",
